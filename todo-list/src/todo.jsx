35d9bfe52e7aaee3b924bfbbd54e9f36fc449f51
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-08-10 20:23:10 -0700

    Completed first task, user can make a todo

---
+++ todo-list/src/todo.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+
+const useInputValue = initialValue => {
+  const [value, setValue] = useState("");
+
+  return {
+    value,
+    onChange: event => setValue(event.target.value),
+    resetValue: () => setValue("")
+  }
+}
+
+export default function TodoForm ({onSubmit}) {
+  const { resetValue, ...todoText } = useInputValue("");
+
+  return (
+    <form
+      onSubmit={event => {
+        event.preventDefault();
+        onSubmit(todoText.value);
+        resetValue();
+      }}
+    >
+      <input className='todo-input' {...todoText} />
+    </form>
+  )
+}
